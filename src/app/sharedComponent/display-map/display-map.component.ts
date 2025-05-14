ddab43a776bbaf4ab7a064cfc39678ee258a1dd7
Nahima697 <nahima.toumi697@gmail.com>
2025-05-15 00:34:51 +0200

    modal base

--- src/app/sharedComponent/display-map/display-map.component.ts
+++ src/app/sharedComponent/display-map/display-map.component.ts
@@ -1,79 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 import { LngLatLike, Map, GeolocateControl } from 'maplibre-gl';
 import { Station, STATION } from '../station/station.component';
 import { FormFieldComponent } from '../form-field/form-field.component';
 import { ControlType } from '../form-field/form-field.enum.';
+import { IonIcon, ModalController, IonContent } from '@ionic/angular/standalone';
+import { FilterModalComponent } from '../filter-modal/filter-modal.component';
+import { MapLibreSearchControl } from "@stadiamaps/maplibre-search-box";
+import "@stadiamaps/maplibre-search-box/dist/maplibre-search-box.css";
 
 @Component({
   selector: 'app-display-map',
   standalone: true,
-  imports: [RouterModule, NgxMapLibreGLModule, FormFieldComponent],
-  template: `
-    <app-form-field
-      label="Rechercher"
-      placeholder="Votre adresse"
-      [controlType]="ControlType.Search"
-      type="search"
-      (ionInput)="onLocalSearch($event)"
-    ></app-form-field>
-
-    <div class="map-wrap">
-      <mgl-map
-        class="map"
-        [style]="'https://api.maptiler.com/maps/streets-v2/style.json?key=pUgm4NlEXg3amyAe53SH'"
-        [zoom]="[14]"
-        [center]="center"
-        (mapLoad)="onMapLoad($event)"
-      >
-        @for (station of filteredStations; track station.id) {
-          <mgl-marker
-            [lngLat]="[station.longitude, station.latitude]"
-            [className]="'charger-marker'"
-          >
-            <div
-              class="charger-marker"
-              (mouseenter)="onHover(station)"
-              (mouseleave)="onLeave()"
-              (click)="onHover(station)"
-            >
-              <img
-                src="assets/icons/battery.png"
-                alt="Borne"
-              />
-            </div>
-          </mgl-marker>
-
-          @if (hoverStation?.id === station.id) {
-            <mgl-popup
-              [lngLat]="[station.longitude, station.latitude]"
-              [closeButton]="true"
-              [closeOnClick]="true"
-              [anchor]="'top'"
-            >
-              <div>
-                <h3>{{ station.nom }}</h3>
-                <p>{{ station.adresse }}</p>
-                <p><strong>Puissance:</strong> {{ station.puissance }}</p>
-              </div>
-            </mgl-popup>
-          }
-        }
-
-        <mgl-control mglNavigation
-        mglGeolocate
-        [positionOptions]="{ enableHighAccuracy: true }"
-        [trackUserLocation]="true"
-        [showUserLocation]="true"
-        position="top-right" />
-
-      </mgl-map>
-    </div>
-  `,
+  imports: [
+    RouterModule,
+    NgxMapLibreGLModule,
+    FormFieldComponent,
+    FilterModalComponent,
+    IonIcon
+  ],
+  templateUrl: './display-map.component.html',
   styleUrls: ['./display-map.component.scss'],
 })
 export class DisplayMapComponent implements OnInit {
+  private router = inject(Router);
+  private modalCtrl = inject(ModalController);
+
   map!: Map;
   stations: Station[] = STATION;
   filteredStations: Station[] = [];
@@ -81,36 +34,23 @@ export class DisplayMapComponent implements OnInit {
   hoverStation?: Station;
   ControlType = ControlType;
 
-
   ngOnInit(): void {
-    this.filteredStations = this.stations;
+    const state = this.getRouterState();
+
+    this.filteredStations = state?.filteredStations?.length
+      ? state.filteredStations
+      : this.stations;
+
+    if (this.filteredStations.length) {
+      this.centerMapOnStation(this.filteredStations[0]);
+    }
   }
 
   onMapLoad(map: Map): void {
     this.map = map;
 
-    const geolocate = new GeolocateControl({
-      positionOptions: { enableHighAccuracy: true },
-      trackUserLocation: true,
-    });
-
-    map.addControl(geolocate);
-
-    setTimeout(() => {
-      try {
-        geolocate.trigger();
-      } catch (err) {
-        console.warn('Erreur lors de la géolocalisation :', err);
-      }
-    }, 1000);
-
-    geolocate.on('geolocate', (position) => {
-      console.log('Position trouvée :', position.coords);
-    });
-
-    geolocate.on('error', () => {
-      console.warn('Erreur lors de la localisation');
-    });
+    this.setupGeolocateControl(map);
+    // this.setupSearchControl(map);
   }
 
   onHover(station: Station): void {
@@ -122,23 +62,89 @@ export class DisplayMapComponent implements OnInit {
   }
 
   onLocalSearch(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
+    const value = (event.target as HTMLInputElement).value?.toLowerCase() || '';
 
-    const value = inputElement.value?.toLowerCase() || '';
     this.filteredStations = this.stations.filter(station =>
       station.nom.toLowerCase().includes(value) ||
       station.adresse.toLowerCase().includes(value)
     );
-    console.log(this.filteredStations);
-    if (this.filteredStations.length > 0 && this.map) {
-      const firstStation = this.filteredStations[0];
+
+    if (this.filteredStations.length) {
+      this.centerMapOnStation(this.filteredStations[0]);
+    }
+  }
+
+  openFilterModal() {
+    this.modalCtrl.create({
+      component: FilterModalComponent,
+      initialBreakpoint: 0.45,
+      breakpoints: [0, 0.25, 0.5, 0.75],
+      cssClass: 'bottom-sheet',
+    }).then(modal => modal.present());
+  }
+
+  onFilterChange(filterValue: string) {
+    this.filteredStations = this.stations.filter(station =>
+      station.nom.toLowerCase().includes(filterValue) ||
+      station.adresse.toLowerCase().includes(filterValue)
+    );
+
+    if (this.filteredStations.length) {
+      this.centerMapOnStation(this.filteredStations[0]);
+    }
+  }
+
+  private centerMapOnStation(station: Station) {
+    this.center = [station.longitude, station.latitude];
+    if (this.map) {
       this.map.flyTo({
-        center: [firstStation.longitude, firstStation.latitude],
+        center: this.center,
         zoom: 15,
         essential: true
       });
     }
-
   }
 
+  private getRouterState(): { filteredStations?: Station[] } | undefined {
+    return this.router.getCurrentNavigation()?.extras?.state as { filteredStations?: Station[] };
+  }
+
+  private setupGeolocateControl(map: Map): void {
+    const state = this.getRouterState();
+    const geolocate = new GeolocateControl({
+      positionOptions: { enableHighAccuracy: true },
+      trackUserLocation: true,
+    });
+
+    map.addControl(geolocate);
+
+    if (!state?.filteredStations?.length) {
+      setTimeout(() => {
+        try {
+          geolocate.trigger();
+        } catch (err) {
+          console.warn('Erreur lors de la géolocalisation :', err);
+        }
+      }, 1000);
+    }
+
+    geolocate.on('geolocate', position => {
+      console.log('Position trouvée :', position.coords);
+    });
+
+    geolocate.on('error', () => {
+      console.warn('Erreur lors de la localisation');
+    });
+  }
+
+  // private setupSearchControl(map: Map): void {
+  //   const control = new MapLibreSearchControl({
+  //     onResultSelected: (feature) => {
+  //       console.log('Résultat sélectionné', feature);
+  //       // TODO: Tu peux ici adapter `this.center` et déclencher un filtre si pertinent
+  //     },
+  //   });
+
+  //   map.addControl(control, 'top-left');
+  // }
 }
