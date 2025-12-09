3109dca98dadf08d0d94afbfe4aa96cf79cf3e4f
Nahima <nahima.toumi697@gmail.com>
2025-12-09 13:25:25 +0100

    update review card et review list

--- src/app/features/review/components/review-card/review-card.component.ts
+++ src/app/features/review/components/review-card/review-card.component.ts
@@ -1,14 +1,29 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonAvatar,
+  IonIcon,
+  IonItem,
+  IonLabel
+} from "@ionic/angular/standalone";
 import { reviewResponseDTO } from '../../models/review.model';
 
 @Component({
   selector: 'app-review-card',
   templateUrl: './review-card.component.html',
   styleUrls: ['./review-card.component.scss'],
+  imports: [
+    DatePipe,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonAvatar,
+    IonIcon
+  ]
 })
-export class ReviewCardComponent  {
-
-readonly review = input.required<reviewResponseDTO>();
-
-
+export class ReviewCardComponent {
+  readonly review = input.required<reviewResponseDTO>();
 }
--- src/app/features/review/components/review-list/review-list.component.ts
+++ src/app/features/review/components/review-list/review-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { reviewResponseDTO } from '../../models/review.model';
 import { ReviewCardComponent } from "../review-card/review-card.component";
+import { IonIcon } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-review-list',
   templateUrl: './review-list.component.html',
   styleUrls: ['./review-list.component.scss'],
-  imports: [ReviewCardComponent],
+  imports: [ReviewCardComponent, IonIcon],
 })
-export class ReviewListComponent  {
-readonly reviews = input.required<reviewResponseDTO[]>();
-
+export class ReviewListComponent {
+  readonly reviews = input.required<reviewResponseDTO[]>();
 }
