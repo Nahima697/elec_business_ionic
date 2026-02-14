7edf1fba1b40b41e2a1a919770072ed95c0fb588
Nahima <nahima.toumi697@gmail.com>
2026-02-14 21:25:43 +0100

    modify style search input

--- src/app/features/display-map/pages/map.page.ts
+++ src/app/features/display-map/pages/map.page.ts
@@ -1,12 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonContent, IonSpinner } from '@ionic/angular/standalone';
+import { IonContent, IonSpinner,IonFooter } from '@ionic/angular/standalone';
 import { DisplayMapComponent } from '../components/display-map/display-map.component';
 import { StationApiService } from 'src/app/features/charging-station/services/station-api.service';
-
+import { HeaderComponent } from 'src/app/shared-component/header/header.component';
+import { FooterComponent } from 'src/app/shared-component/footer/footer.component';
 @Component({
   selector: 'app-map',
   template: `
+  <app-header class="ion-hide-md-down" [stations]="stations.error() ? [] : (stations.value() ?? [])"
+    [(open)]="openMenuMobile"></app-header>
     <ion-content [fullscreen]="true">
       @if (stations.isLoading()) {
         <div class="spinner-container">
@@ -20,6 +23,9 @@ import { StationApiService } from 'src/app/features/charging-station/services/st
         </app-display-map>
       }
     </ion-content>
+    <ion-footer class="ion-hide-md-down">
+      <app-footer></app-footer>
+    </ion-footer>
   `,
   styles: [`
     .spinner-container {
@@ -30,11 +36,12 @@ import { StationApiService } from 'src/app/features/charging-station/services/st
     }
   `],
   standalone: true,
-  imports: [DisplayMapComponent, IonContent, IonSpinner]
+  imports: [DisplayMapComponent, IonContent, IonSpinner,HeaderComponent, FooterComponent,IonFooter]
 })
 export class MapPage {
   private stationService = inject(StationApiService);
   private router = inject(Router);
+  protected readonly openMenuMobile= signal(false);
 
   readonly stations = this.stationService.getAll();
 
@@ -43,6 +50,7 @@ export class MapPage {
   searchFromNav: string | undefined;
 
   constructor() {
+    // On capture l'état pendant la navigation (C'est le SEUL endroit fiable)
     const nav = this.router.getCurrentNavigation();
     const state = nav?.extras.state as { center?: [number, number], searchTerm?: string };
 
